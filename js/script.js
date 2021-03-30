ac4b4b2627a1b8dc6ac32cbc7c5d6a3706732630
Tommaso P <tommaso.plinio@gmail.com>
2021-03-30 15:10:10 +0200

    v for index ok

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@
 // nome e immagine di ogni contatto
 
 
+// Milestone 2
+// ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// ● Click sul contatto mostra la conversazione del contatto cliccato
+
 
 
 var app = new Vue ({
@@ -101,6 +105,13 @@ var app = new Vue ({
               }
             ],
           },
-        ]
+        ],
+      contactIndex: 0
     },
+    methods: {
+      selectContact: function(i) {
+        this.contactIndex = i;
+        console.log(i);
+      }
+    }
 });
